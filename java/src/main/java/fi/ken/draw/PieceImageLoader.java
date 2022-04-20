bc2f5360af9e03134a34761552fd9707de869695
KenErikson <redacted@example.com>
2022-04-20 21:42:58 +0300

    (java) #3 Load chess images

---
+++ java/src/main/java/fi/ken/draw/PieceImageLoader.java
@@ -0,0 +1,14 @@
+package fi.ken.draw;
+
+import java.nio.file.Path;
+
+import fi.ken.chess.Team;
+import fi.ken.chess.piece.Piece;
+
+public class PieceImageLoader {
+
+    public static Path getImagePathFor( Piece piece, Team team ){
+        String filename = "Chess_";
+        System.class.getResource()
+    }
+}
